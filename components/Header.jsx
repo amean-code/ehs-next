3991fef1589ff601ff906617da536038865935b1
muhammedkartci <muhammed.kartci@gmail.com>
2023-10-06 23:59:25 +0300

    Slider - Header - Team are added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,74 +1,49 @@
 import Image from "next/image";
 import { useState } from "react";
 import { Arrow_down } from "../icons";
+import styles from "../styles/header.module.css";
 
 function Header() {
   const [activeNav, setActiveNav] = useState("");
 
   return (
-    <header className="py-[1rem] px-[10%] bg-white border-b-[0.5rem] border-primary-dark">
-      <div className="flex items-center justify-between bsm:justify-center">
-        <div className="">
-          <img
-            src="/images/academy-logo-white-border-black.svg"
-            alt="AMEAN Academy Logo"
-            className="w-[350px] h-auto bsm:w-[200px]"
-          />
+    <header className={styles.site_header} >
+      <div className={styles.header_top}>
+        <div className={styles.header_top_left}>
+          <div className={styles.whatsapp}>
+            <Image width={20} height={20} className={styles.whatsapp_logo} src="/images/whatsapp.svg" alt="" />
+            <p>+90 (123) 123 12 12</p>
+          </div>
+          <div className={styles.email}>
+            <Image width={20} height={20} className={styles.email_logo} src="/images/gmail.svg" alt="" />
+            <p>ehs@gmail.com</p>
+          </div>
+        </div>
+        <div className={styles.header_top_right}>
+          <div className={styles.languages}>
+            <Image width={200} height={200} src="/images/tr-buton.svg" alt="" />
+            <Image width={200} height={200} src="/images/us-buton.svg" alt="" />
+          </div>
+          <div className={styles.social_media}>
+            <Image width={200} height={200} src="/images/linkedin.svg" alt="" />
+            <Image width={200} height={200} src="/images/instagram.svg" alt="" />
+            <Image width={200} height={200} src="/images/youtube.svg" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className={styles.header_container}>
+        <div className={styles.header_left}>
+          <div className={styles.header_logo}>
+            <Image width={200} height={200} src="/images/logo.png" alt="" />
+          </div>
+        </div>
+        <div className={styles.header_right}>
+          <a href="anasayfa.php">About Us</a>
+          <a href="hakkimizda.php">Events</a>
+          <a href="contact.php">Mentoring</a>
+          <a href="prof.php">Contact Us</a>
         </div>
 
-        <nav className="flex items-center justify-evenly">
-          <ul className="flex list-none mb-0 mr-[5px] pr-[1rem] border-r-[1px] border-secondary">
-            <li
-              className="relative mr-[1.5rem] "
-              onMouseOver={() => {
-                setActiveNav("Başkan");
-              }}
-              onMouseLeave={() => {
-                setActiveNav("");
-              }}
-            >
-              <span className="font-[400] flex items-center duration-300 ease hover:scale-110 cursor-pointer">
-                Projelerimiz <Arrow_down classname="" />
-              </span>
-              <ul
-                className={`min-w-[15rem] py-[0.5rem] my-[0.125rem] text-left list-none bg-white border border-gray-300 rounded-md  z-[1000] top-0 left-0 translate-y-[24px] ${
-                  activeNav === "Başkan" ? "absolute" : "hidden"
-                }`}
-              >
-                <li>
-                  <a
-                    href=""
-                    className="select-none hover:bg-gray-100 block w-full py-[0.25rem] px-[1.5rem] font-[400] text-[1rem]"
-                  >
-                    Özgeçmiş
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href=""
-                    className="select-none hover:bg-gray-100 block w-full py-[0.25rem] px-[1.5rem] font-[400] text-[1rem]"
-                  >
-                    Galeri
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href=""
-                    className="select-none hover:bg-gray-100 block w-full py-[0.25rem] px-[1.5rem] font-[400] text-[1rem]"
-                  >
-                    Vizyon & Misyon
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-
-          <div className="relative">
-            <a className=" font-[600] text-center cursor-pointer select-none text-[1rem] no-underline">
-              EN
-            </a>
-          </div>
-        </nav>
       </div>
     </header>
   );
