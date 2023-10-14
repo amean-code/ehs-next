1813263f72d688a194d0ba69a86ca2dbbcaca5b4
muhammedkartci <muhammed.kartci@gmail.com>
2023-10-14 08:34:17 +0300

    Edited links

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -8,37 +8,37 @@ function Contact() {
 
   return (
     <div class="flex w-full p-[24px] justify-center items-center">
-      <div class="flex flex-col md:flex-row md:space-y-0 bg-green-600  p-8 sm:p-12 rounded-xl shadow-lg text-white overflow-hidden">
+      <div class="flex flex-col md:flex-row  gap-5 md:space-y-0 bg-green-600  p-8 sm:p-12 rounded-xl shadow-lg text-white overflow-hidden">
         <div class="flex flex-col space-y-8 justify-between">
           <div>
             <h1 class="font-bold text-4xl tracking-wide">Contact Us</h1>
-            <p class="pt-2 text-green-100 text-sm">For all your non-existant company needs Fake Corp are here to help, do not hesitate to contact us!</p>
+            <p class="pt-2 text-green-100 text-sm">For all your non-existant company needs Fake Corp are here to help, do not<br/> hesitate to contact us!</p>
           </div>
           <div class="flex flex-col space-y-6">
             <div class="inline-flex space-x-2 items-center w-60">
               <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
-              <span>+111 345 214 213</span>
+              <span>+123 123 12 12</span>
             </div>
             <div class="inline-flex space-x-2 items-center">
               <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
-              <span>hello@fakecorp.com</span>
+              <span>info@ehs.com</span>
             </div>
             <div class="inline-flex space-x-2 items-center">
               <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>
-              <span>111 Fake Boulevard, Fakesville, FYC</span>
+              <span>111 Fake Boulevard, İstanbul, TURKEY</span>
             </div>
           </div>
           <div class="flex space-x-4">
-            <a href="">
+            <a href="https://www.facebook.com/" target="blank_">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
             </a>
-            <a href="">
+            <a href="https://www.instagram.com/" target="blank_">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
             </a>
-            <a href="">
+            <a href="https://twitter.com/" target="blank_">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
             </a>
-            <a href="">
+            <a href="https://www.linkedin.com/" target="blank_">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
             </a>
           </div>
