739e8294d3125d96fd072e4d280e28a91b244168
muhammedkartci <muhammed.kartci@gmail.com>
2023-10-16 00:07:05 +0300

    Mentroing - footer edited

--- components/Mentoring.jsx
+++ components/Mentoring.jsx
@@ -29,7 +29,7 @@ function Mentoring() {
           </div>
           <div className={styles.col_lg_4 + " " + styles.col_md_6 + " " + styles.mb30}>
             <div className={styles.f_box + " " + styles.f_icon_left + " " + styles.f_icon_rounded}>
-              <i className={styles.fa + " " + styles.fa_book + " " + styles.id_color + " " + styles.bg_color_op_1}></i>
+              <i className={styles.fa + " " + styles.fa_pencil + " " + styles.id_color + " " + styles.bg_color_op_1+" "+styles.i}></i>
               <div className={styles.fb_text}>
                 <h4 className={styles.h4}>Experential Learning</h4>
                 <p className={styles.p}>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem.
@@ -39,7 +39,7 @@ function Mentoring() {
           </div>
           <div className={styles.col_lg_4 + " " + styles.col_md_6 + " " + styles.mb30}>
             <div className={styles.f_box + " " + styles.f_icon_left + " " + styles.f_icon_rounded}>
-              <i className={styles.fa + " " + styles.fa_book + " " + styles.id_color + " " + styles.bg_color_op_1}></i>
+              <i className={styles.fa + " " + styles.fa_search + " " + styles.id_color + " " + styles.bg_color_op_1+" "+styles.i}></i>
               <div className={styles.fb_text}>
                 <h4 className={styles.h4}>Self Discovery</h4>
                 <p className={styles.p}>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem.
@@ -49,7 +49,7 @@ function Mentoring() {
           </div>
           <div className={styles.col_lg_4 + " " + styles.col_md_6 + " " + styles.mb30}>
             <div className={styles.f_box + " " + styles.f_icon_left + " " + styles.f_icon_rounded}>
-              <i className={styles.fa + " " + styles.fa_book + " " + styles.id_color + " " + styles.bg_color_op_1}></i>
+              <i className={styles.fa + " " + styles.fa_camera + " " + styles.id_color + " " + styles.bg_color_op_1+" "+styles.i}></i>
               <div className={styles.fb_text}>
                 <h4 className={styles.h4}>Inspirational Movie</h4>
                 <p className={styles.p}>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem.
@@ -59,7 +59,7 @@ function Mentoring() {
           </div>
           <div className={styles.col_lg_4 + " " + styles.col_md_6 + " " + styles.mb30}>
             <div className={styles.f_box + " " + styles.f_icon_left + " " + styles.f_icon_rounded}>
-              <i className={styles.fa + " " + styles.fa_book + " " + styles.id_color + " " + styles.bg_color_op_1}></i>
+              <i className={styles.fa + " " + styles.fa_gears + " " + styles.id_color + " " + styles.bg_color_op_1+" "+styles.i}></i>
               <div className={styles.fb_text}>
                 <h4 className={styles.h4}>Simulation</h4>
                 <p className={styles.p}>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem.
@@ -69,7 +69,7 @@ function Mentoring() {
           </div>
           <div className={styles.col_lg_4 + " " + styles.col_md_6 + " " + styles.mb30}>
             <div className={styles.f_box + " " + styles.f_icon_left + " " + styles.f_icon_rounded}>
-              <i className={styles.fa + " " + styles.fa_book + " " + styles.id_color + " " + styles.bg_color_op_1}></i>
+              <i className={styles.fa + " " + styles.fa_child + " " + styles.id_color + " " + styles.bg_color_op_1+" "+styles.i}></i>
               <div className={styles.fb_text}>
                 <h4 className={styles.h4}>Fun Games</h4>
                 <p className={styles.p}>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem.
