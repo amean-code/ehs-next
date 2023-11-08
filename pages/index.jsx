dcb16cb72ca0162ece452b242a67cd2416046109
DurmushKartci <durmusikartci@gmail.com>
2023-11-09 00:49:37 +0300

    events component yazıldı

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,18 +7,26 @@ import Team from "../components/Team";
 import TimeLine from "../components/TimeLine";
 import NotFoundPage from "./404";
 import Vision from "../components/Vision";
+import Events from "../components/Events";
 import News from "../components/News";
 import Blog from "../components/Blog";
 import Mentoring from "../components/Mentoring";
 import styles from "../styles/App.module.css"
+import Head from "next/head";
 export default function Home() {
   return (
     
     <div className={styles.main_body}>
+      <Head>
+        <title>
+          Energy Hack Space
+        </title>
+      </Head>
       <Header />
       <Slider />
       <SliderBottom />
       <Vision/>
+      <Events/>
       <News/>
       <Mentoring/>
       <Team />
