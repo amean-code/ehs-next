cf15cb2f97604c7e605799313dfd23b28bea62e0
eminkartci <emin.kartci@ozu.edu.tr>
2023-11-09 10:13:14 +0300

    sıralama güncellendi

--- pages/index.jsx
+++ pages/index.jsx
@@ -25,8 +25,8 @@ export default function Home() {
       <Header />
       <Slider />
       <SliderBottom />
-      <Events />
       <TimeLine />
+      <Events />
       <News/>
       <Mentoring/>
       <Team />
