30266c61b644164593fa9e65e4fabddafe553f67
muhammedkartci <muhammed.kartci@gmail.com>
2023-11-29 22:36:21 +0300

    Hackathona ödüller kısmı eklendi - Hakemler kısmına başlandı

--- components/Hackathon.jsx
+++ components/Hackathon.jsx
@@ -59,113 +59,84 @@ function Hackathon() {
                 </div>
             </div>
             {/* SLIDER */}
-            <div className={styles.main_container}>
+                <div className={styles.main_container}>
 
-                <div className={styles.containRight}>
-                    <h4>
-                        {t?.ehs}
-                    </h4>
-                    <h2>{t?.gorevler_title}</h2>
-                    <div className={styles.buttons}>
-                        <div onClick={() => set_active_button("who-we-are")} className={styles.item + " " + (
+                    <div className={styles.containRight}>
+                        <h4>
+                            {t?.ehs}
+                        </h4>
+                        <h2>{t?.gorevler_title}</h2>
+                        <div className={styles.buttons}>
+                            <div onClick={() => set_active_button("who-we-are")} className={styles.item + " " + (
+                                active_button == "who-we-are" ?
+                                    styles.active : ""
+                            )}>
+                                {t?.gorev1}
+                            </div>
+                            <div onClick={() => set_active_button("what-we-do")} className={styles.item + " " + (
+                                active_button == "what-we-do" ?
+                                    styles.active : ""
+                            )}>
+                                {t?.gorev2}
+                            </div>
+                            <div onClick={() => set_active_button("why-join")} className={styles.item + " " + (
+                                active_button == "why-join" ?
+                                    styles.active : ""
+                            )}>
+                                {t?.gorev3}
+                            </div>
+                        </div>
+                        {
                             active_button == "who-we-are" ?
-                                styles.active : ""
-                        )}>
-                            {t?.gorev1}
+                                        <p>
+                                    {t?.who_we_are_text}
+                                </p>
+                                : active_button == "what-we-do" ?
+                                    <p>
+                                        {t?.what_we_do_text}
+                                    </p>
+                                    : active_button == "why-join" ?
+                                        <p>
+                                            {t?.why_join_text}
+                                        </p>
+                                        : ""
+                        }
+                    </div>
+                    <div className={styles.containLeft}>
+                        <Image className={styles.slider_imageleft} width={600} height={600} src="/images/shape.jpeg" alt="" />
+                    </div>
+                </div>  
+            {/*SLIDER*/}
+
+            {/*ODULLER*/}
+                <div className={styles.prizes}>
+                    <p>{t?.oduller}</p>
+                    <div className={styles.prize}>
+                        <div className={styles.item}>
+                            <img src="/images/second.png" alt="" />
+                            <p className={styles.price}>$350</p>
+                            <p>{t?.per_person}</p>
                         </div>
-                        <div onClick={() => set_active_button("what-we-do")} className={styles.item + " " + (
-                            active_button == "what-we-do" ?
-                                styles.active : ""
-                        )}>
-                            {t?.gorev2}
+                        <div className={styles.item}>
+                            <img src="/images/first.png" alt="" />
+                            <p className={styles.price}>$500</p>
+                            <p>{t?.per_person}</p>
                         </div>
-                        <div onClick={() => set_active_button("why-join")} className={styles.item + " " + (
-                            active_button == "why-join" ?
-                                styles.active : ""
-                        )}>
-                            {t?.gorev3}
+                        <div className={styles.item}>
+                            <img src="/images/third.png" alt="" />
+                            <p className={styles.price}>$250</p>
+                            <p>{t?.per_person}</p>
                         </div>
                     </div>
-                    {
-                        active_button == "who-we-are" ?
-                            <p>
-                                {t?.who_we_are_text}
-                            </p>
-                            : active_button == "what-we-do" ?
-                                <p>
-                                    {t?.what_we_do_text}
-                                </p>
-                                : active_button == "why-join" ?
-                                    <p>
-                                        {t?.why_join_text}
-                                    </p>
-                                    : ""
-                    }
                 </div>
-                <div className={styles.containLeft}>
-                    <Image className={styles.slider_imageleft} width={200} height={200} src="/images/shape.jpeg" alt="" />
+            {/*ODULLER*/}
+            {/*HAKEMLER*/}
+                <div className={styles.judge}>
+                    <div className={styles.title}>
+                        <p>{t?.judge}</p>
+                    </div>
                 </div>
-            </div>
-            {/*SLIDER*/}
-            <div className={styles.text_center}>
-          <h2 className={styles.title_h2}>Our Team</h2>
-          <div className={styles.small_border}></div>
-        </div>
-
-        
-        <div className={styles.cardArea}>
-          {/* SEMANUR */}
-          <div className={styles.cardRight}>
-            <figure className={styles.snip1192}>
-              <div className={styles.author}>
-                <img src="https://st.ourhtmldemo.com/new/Life_Guide/images/testimonial/1.png" alt="sq-sample1" />
-                <div className={styles.linkedinArea}>
-                <Link href="#"><i className="fa-brands fa-linkedin fa-xl w-[24px] h-[24px]" style={{ color: "#166bfe" }}></i></Link>
-                {/* <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/turan-güzel-6383aa226/">Turan Mete Güzel</a> */}
-              </div>
-                <h5 className={styles.h5}>Semanur Sancar <span className={styles.span}>Founding Partner & Academy Manager</span></h5>
-              </div>
-              <p className={styles.briefbioText}>Must explain to you how mistaken idea denouncing pleasure & praising pain was born & we will give expound actual human happiness.</p>
-             
-            </figure>
-          </div>
-
-          
-        </div>
-
-        
-        <div className={styles.cardArea}>
-          {/* OPERASYON */}
-          <div className={styles.cardRight}>
-            <figure className={styles.snip1192}>
-              <div className={styles.author}>
-                <img src="https://st.ourhtmldemo.com/new/Life_Guide/images/testimonial/1.png" alt="sq-sample1" />
-                <div className={styles.linkedinArea}>
-                <Link href="#"><i className="fa-brands fa-linkedin fa-xl w-[24px] h-[24px]" style={{ color: "#166bfe" }}></i></Link>
-                {/* <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/turan-güzel-6383aa226/">Turan Mete Güzel</a> */}
-              </div>
-                <h5 className={styles.h5}> ######## <span className={styles.span}>Operations Manager</span></h5>
-              </div>
-              <p className={styles.briefbioText}>Must explain to you how mistaken idea denouncing pleasure & praising pain was born & we will give expound actual human happiness.</p>
-             
-            </figure>
-          </div>
-
-          {/* ILETISIM */}
-          <div className={styles.cardRight}>
-            <figure className={styles.snip1192}>
-              <div className={styles.author}>
-                <img src="https://st.ourhtmldemo.com/new/Life_Guide/images/testimonial/1.png" alt="sq-sample1" />
-                <h5 className={styles.h5}>Teena Venanda <span className={styles.span}>CEO/Founder</span></h5>
-              </div>
-              <p className={styles.briefbioText}>Must explain to you how mistaken idea denouncing pleasure & praising pain was born & we will give expound actual human happiness.</p>
-              <div className={styles.linkedinArea}>
-                <Link href="#"><i className="fa-brands fa-linkedin fa-xl w-[24px] h-[24px]" style={{ color: "#166bfe" }}></i></Link>
-                {/* <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/turan-güzel-6383aa226/">Turan Mete Güzel</a> */}
-              </div>
-            </figure>
-          </div>
-        </div>
+            {/*HAKEMLER*/}
         </section>
     );
 }
