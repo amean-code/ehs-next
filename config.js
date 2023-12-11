09ea6d62cb4c374f5a14216112ba7be0dc437c2e
DurmushKartci <durmusikartci@gmail.com>
2023-12-12 00:37:32 +0300

    application form

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const Config = {
     AppName: 'EHS',
     ServerAPI: 'http://localhost:4511',
+    StrapiAPI: 'http://localhost:1337',
     // ServerAPI: 'https://bcevpro.ameandanismanlik.com',
     // ServerAPI: 'https://backend.trevpro.com',
 }
