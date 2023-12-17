957985706fa1a019fbd13391c94f71f57166c1a5
DurmushKartci <durmusikartci@gmail.com>
2023-12-17 23:33:16 +0300

    applciation form düzenlendi

--- components/Mentoring.jsx
+++ components/Mentoring.jsx
@@ -30,8 +30,11 @@ function Mentoring() {
             <div className={styles.f_box + " " + styles.f_icon_left + " " + styles.f_icon_rounded}>
               <i className={styles.fa + " " + styles.fa_book + " " + styles.id_color + " " + styles.bg_color_op_1+" "+styles.i}></i>
               <div className={styles.fb_text}>
-                <h4 className={styles.h4}>{t?.edutainment}</h4>
-                <p className={styles.p}>{t?.lorem}
+                <h4 className={styles.h4}>
+                  Lisans ve Lisansüstü Tez Mentörlüğü
+                </h4>
+                <p className={styles.p}>
+                  Energy Hack Space, lisans ve lisansüstü tez çalışmaları sürecinde öğrencilere yol gösterir. Özellikle enerji alanında inovasyon ve sürdürülebilirlik konularına odaklanan bu mentörlük, öğrencilere tez konularını belirlemeden araştırma metodolojilerini oluşturmaya kadar geniş bir perspektif sunar. Bilimsel literatürün analizi, veri toplama yöntemleri ve sonuçların yorumlanması konularında destek sağlar.
                 </p>
               </div>
             </div>
@@ -40,8 +43,11 @@ function Mentoring() {
             <div className={styles.f_box + " " + styles.f_icon_left + " " + styles.f_icon_rounded}>
               <i className={styles.fa + " " + styles.fa_pencil + " " + styles.id_color + " " + styles.bg_color_op_1+" "+styles.i}></i>
               <div className={styles.fb_text}>
-                <h4 className={styles.h4}>{t?.experential_learning}</h4>
-                <p className={styles.p}>{t?.lorem}
+                <h4 className={styles.h4}>
+                  Proje Mentörlüğü
+                </h4>
+                <p className={styles.p}>
+                  Energy Hack Space, enerji sektöründeki projelerde öncü ve yenilikçi fikirlerin hayata geçirilmesi için rehberlik eder. Proje fikrinin oluşumundan uygulamaya kadar olan sürecin her aşamasında, teknik danışmanlığın yanı sıra stratejik ve operasyonel önerilerle projenin başarılı bir şekilde tamamlanmasına katkıda bulunur.
                 </p>
               </div>
             </div>
@@ -50,8 +56,11 @@ function Mentoring() {
             <div className={styles.f_box + " " + styles.f_icon_left + " " + styles.f_icon_rounded}>
               <i className={styles.fa + " " + styles.fa_search + " " + styles.id_color + " " + styles.bg_color_op_1+" "+styles.i}></i>
               <div className={styles.fb_text}>
-                <h4 className={styles.h4}>{t?.self_discovery}</h4>
-                <p className={styles.p}>{t?.lorem}
+                <h4 className={styles.h4}>
+                  Akademik ve Kariyer Mentörlüğü
+                </h4>
+                <p className={styles.p}>
+                  Energy Hack Space, enerji sektöründe kariyer yapmayı hedefleyen bireyler için akademik ve kariyer yollarını şekillendirme konusunda rehberlik sunar. Eğitim seçimleri, staj ve iş başvuruları, kariyer planlama ve profesyonel gelişim stratejileri gibi konularda deneyimli mentörlerin yönlendirmesiyle bireylerin kariyer hedeflerine ulaşmalarına yardımcı olur.
                 </p>
               </div>
             </div>
@@ -60,8 +69,11 @@ function Mentoring() {
             <div className={styles.f_box + " " + styles.f_icon_left + " " + styles.f_icon_rounded}>
               <i className={styles.fa + " " + styles.fa_camera + " " + styles.id_color + " " + styles.bg_color_op_1+" "+styles.i}></i>
               <div className={styles.fb_text}>
-                <h4 className={styles.h4}>{t?.inspirational_movie}</h4>
-                <p className={styles.p}>{t?.lorem}
+                <h4 className={styles.h4}>
+                  Girişimcilik Mentörlüğü
+                </h4>
+                <p className={styles.p}>
+                  Energy Hack Space, enerji sektöründe girişimci olmayı düşünen bireyler için vizyon oluşturma, iş modeli geliştirme ve pazar analizi gibi konularda rehberlik eder. Girişimciliğin zorluklarına ve fırsatlarına dair gerçekçi bir perspektif sunarak, girişimcilerin sürdürülebilir ve başarılı işletmeler kurmaları için gereken bilgi ve becerilere erişmelerini sağlar.
                 </p>
               </div>
             </div>
@@ -70,18 +82,11 @@ function Mentoring() {
             <div className={styles.f_box + " " + styles.f_icon_left + " " + styles.f_icon_rounded}>
               <i className={styles.fa + " " + styles.fa_gears + " " + styles.id_color + " " + styles.bg_color_op_1+" "+styles.i}></i>
               <div className={styles.fb_text}>
-                <h4 className={styles.h4}>{t?.simulation}</h4>
-                <p className={styles.p}>{t?.lorem}
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className={styles.col_lg_4 + " " + styles.col_md_6 + " " + styles.mb30}>
-            <div className={styles.f_box + " " + styles.f_icon_left + " " + styles.f_icon_rounded}>
-              <i className={styles.fa + " " + styles.fa_child + " " + styles.id_color + " " + styles.bg_color_op_1+" "+styles.i}></i>
-              <div className={styles.fb_text}>
-                <h4 className={styles.h4}>{t?.fun_games}</h4>
-                <p className={styles.p}>{t?.lorem}
+                <h4 className={styles.h4}>
+                  Akran Mentörlüğü
+                </h4>
+                <p className={styles.p}>
+                  Energy Hack Space, enerji sektöründeki profesyonellerin yanı sıra üniversite öğrencilerinin de birbirleriyle bilgi ve deneyimlerini paylaşmaları için bir platform sunar. Akran mentörlüğü, deneyimli profesyonellerden yeni başlayanlara, lisans ve lisansüstü öğrencilerinden mezunlara kadar farklı deneyim seviyelerindeki bireylerin kendi deneyimlerini, başarılarını ve zorluklarını paylaşarak birbirlerine ilham vermesini ve rehberlik etmesini teşvik eder. Özellikle üniversite öğrencileri için, sektöre ilk adımlarını atan veya akademik çalışmalarında yön arayan gençler için bu yaklaşım, hem profesyonel hem de akademik gelişimi destekler ve enerji sektöründe işbirliği ve dayanışma ruhunu teşvik eder.
                 </p>
               </div>
             </div>
